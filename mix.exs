11050ccbeccfaa43ee611560e53ce0b1b9ff73c0
Jon Gautsch <jtgautsch@gmail.com>
2020-09-04 11:09:57 -0700

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,12 +1,21 @@
 defmodule Documo.MixProject do
   use Mix.Project
 
+  @name "Documo"
+  @version "0.1.0"
+  @repo_url "https://github.com/jgautsch/documo-elixir"
+
   def project do
     [
       app: :documo_elixir,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.7",
+      description: "A client lib for Documo's API.",
+      package: package(),
+      docs: docs(),
       start_permanent: Mix.env() == :prod,
+      name: @name,
+      source_url: @repo_url,
       deps: deps()
     ]
   end
@@ -25,8 +34,24 @@ defmodule Documo.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:httpoison, "~> 1.7"},
       {:jason, "~> 1.2"},
-      {:bypass, "~> 2.0"},
-      {:excoveralls, "~> 0.13.1"}
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:bypass, "~> 2.0", only: :test},
+      {:excoveralls, "~> 0.13.1", only: :test}
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo_url}
+    ]
+  end
+
+  def docs do
+    [
+      source_ref: "v#{@version}",
+      source_url: @repo_url,
+      main: @name
     ]
   end
 end
